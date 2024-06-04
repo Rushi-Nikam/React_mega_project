f0be0f9e2a8c910bbef0580927aa39b632d228b6
Rushi <rushidnikam00@gmail.com>
2024-06-04 20:06:33 +0530

    adding some code

--- MegaBlogProject/src/App.jsx
+++ MegaBlogProject/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
 
 function App() {
-
+// console.log(import.meta.env.VITE_APP_APPWRITE_URL)// create vite@latest
+// console.log(process.env.REACT_APP_APPWRITE_URL)// create-react-app
 
   return (
     <>
